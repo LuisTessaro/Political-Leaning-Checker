8eacc8ee1a72def55b25a43f9c6523bf54f4fd8f
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2019-05-28 21:02:16 -0300

    example

---
+++ texts/right.js
@@ -0,0 +1,7 @@
+module.exports = [
+  `Cadê o Rafinha Bostos pra dizer que o Bandhido Dando Boura não sai de casa??
+Seu ilícito`,
+  `Foi muito legal a manifestação
+Foi legal ver você discursando ao vivo amigo `,
+`Nando talvez nao se de conta do que realmente se tornou... estamos com vc Nando`
+]
